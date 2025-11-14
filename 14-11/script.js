94ef547a851a2128b6a0f25deb5e38c21da850ec
Tizzzio <andryzito01@gmail.com>
2025-11-14 16:56:23 +0100

    Aggiunta di un esempio di utilizzo di console.table e creazione di un nuovo file per prompt e alert con aggiunta file script.js

---
+++ 14-11/script.js
@@ -0,0 +1,9 @@
+// Esempio di utilizzo di prompt per ottenere l'input dell'utente
+//const nome = prompt("Come ti chiami?");
+//console.log("Nome inserito:", nome);
+//alert("Ciao, " + nome + "! Benvenuto nel mondo di JavaScript.");
+
+function salutaUtente() {
+  let nomeUtente = prompt("Inserisci il tuo nome:");
+  alert("Ciao, " + nomeUtente + "! Benvenuto nel mondo di JavaScript.");
+}
